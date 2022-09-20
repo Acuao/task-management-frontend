9a497e3ee6d52545682d0fa5be7679b7b2477058
Acuao <Acuao@users.noreply.github.com>
2022-09-20 22:06:58 +0000

    deploy: 761a7a471ce8c419ed85b7b516d51983e5464a64

---
+++ precache-manifest.907ea1888b4aabae8690828c74eda09d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a281d9a2c6508b9f5a4b40d310a161",
+    "url": "https://acuao.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "a6bbc1fa48b6dfd4e112",
+    "url": "https://acuao.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "edd7589e3edaf89e95d8",
+    "url": "https://acuao.github.io/task-management-frontend/static/js/2.804d96b3.chunk.js"
+  },
+  {
+    "revision": "a6bbc1fa48b6dfd4e112",
+    "url": "https://acuao.github.io/task-management-frontend/static/js/main.20f0ee1b.chunk.js"
+  },
+  {
+    "revision": "6a6351989a2c9f130654",
+    "url": "https://acuao.github.io/task-management-frontend/static/js/runtime~main.607a6816.js"
+  }
+]);
